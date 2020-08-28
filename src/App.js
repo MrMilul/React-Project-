6336b246df72990aaf30908f30f41089c9238e70
Mr.Milul <miladmizani@yahoo.com>
2020-08-28 09:11:31 +0300

    footer is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import './App.css';
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 function App() {
   return (
     <>
     <Navbar/>
+    <Footer/>
     </>
   );
 }
