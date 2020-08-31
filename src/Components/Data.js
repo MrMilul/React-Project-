a32755086925b19aff2fae9f97b1d676809ac1af
Mr.Milul <miladmizani@yahoo.com>
2020-08-31 10:21:07 +0300

    data was built

---
+++ src/Components/Data.js
@@ -0,0 +1,103 @@
+export const placeInfo = [
+     {
+          id: 1,
+          headertilte: 'header title...', 
+          headersubtilte: 'header subtitle...', 
+          headetext: 'header text...',
+          title: 'title ...', 
+          description: 'description ...',
+          img: './img/firstpic.jpg',
+          maps: 'Address'
+     },
+     {
+          id: 2,
+          headertilte: 'header title...', 
+          headersubtilte: 'header subtitle...', 
+          headetext: 'header text...',
+          title: 'title ...', 
+          description: 'description ...',
+          img: './img/secondpic.jpg',
+          maps: 'Address'
+     },
+     {
+          id: 3,
+          headertilte: 'header title...', 
+          headersubtilte: 'header subtitle...', 
+          headetext: 'header text...',
+          title: 'title ...', 
+          description: 'description ...',
+          img: './img/thirdpic.jpg',
+          maps: 'Address'
+     },
+     {
+          id: 4,
+          headertilte: 'header title...', 
+          headersubtilte: 'header subtitle...', 
+          headetext: 'header text...',
+          title: 'title ...', 
+          description: 'description ...',
+          img: './img/fouthpic.jpg',
+          maps: 'Address'
+     },
+]
+
+export const detailInfo = {
+     id: 1,
+     headertilte: 'header title...', 
+     headersubtilte: 'header subtitle...', 
+     headetext: 'header text...',
+     title: 'title ...', 
+     description: 'description ...',
+     img: './img/firstpic.jpg',
+     maps: 'Address'
+}
+
+export const news = [
+     {
+          id:1,
+          newsTitle: 'news Title', 
+          newsText: 'news Text '
+     },
+     {
+          id:2,
+          newsTitle: 'news Title', 
+          newsText: 'news Text '
+     },
+     {
+          id:3,
+          newsTitle: 'news Title', 
+          newsText: 'news Text '
+     },
+     {
+          id:4,
+          newsTitle: 'news Title', 
+          newsText: 'news Text '
+     },
+]
+
+export const reviews = [
+     {
+          id:1,
+          name: 'first Person', 
+          avatar: './img/firstavatar.png',
+          comment: 'first person comment'
+     },
+     {
+          id:2,
+          name: 'first Person', 
+          avatar: './img/firstavatar.png',
+          comment: 'second person comment'
+     },
+     {
+          id:3,
+          name: 'first Person', 
+          avatar: './img/firstavatar.png',
+          comment: 'third person comment'
+     },
+     {
+          id:4,
+          name: 'first Person', 
+          avatar: './img/firstavatar.png',
+          comment: 'fourth person comment'
+     },
+]
