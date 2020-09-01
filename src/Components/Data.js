4d6556c76bb7b89b39ea89ca70cf3decf7cf2817
Mr.Milul <miladmizani@yahoo.com>
2020-09-01 17:01:20 +0300

    rectifying some mistakes

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -11,50 +11,50 @@ export const placeInfo = [
      },
      {
           id: 2,
-          headertilte: 'header title...', 
-          headersubtilte: 'header subtitle...', 
-          headetext: 'header text...',
-          title: 'title ...', 
+          headertilte: 'header title2...', 
+          headersubtilte: 'header subtitle2...', 
+          headetext: 'header text2...',
+          title: 'title2 ...', 
           description: 'description ...',
           img: './img/secondpic.jpg',
           maps: 'Address'
      },
      {
           id: 3,
-          headertilte: 'header title...', 
-          headersubtilte: 'header subtitle...', 
-          headetext: 'header text...',
-          title: 'title ...', 
-          description: 'description ...',
+          headertilte: 'header title3...', 
+          headersubtilte: 'header subtitle3...', 
+          headetext: 'header text3...',
+          title: 'title3 ...', 
+          description: 'description3 ...',
           img: './img/thirdpic.jpg',
           maps: 'Address'
      },
      {
           id: 4,
-          headertilte: 'header title...', 
-          headersubtilte: 'header subtitle...', 
-          headetext: 'header text...',
-          title: 'title ...', 
+          headertilte: 'header title4...', 
+          headersubtilte: 'header subtitle4...', 
+          headetext: 'header text4...',
+          title: 'title4 ...', 
           description: 'description ...',
           img: './img/fouthpic.jpg',
           maps: 'Address'
      },
      {
           id: 7,
-          headertilte: 'header title...', 
-          headersubtilte: 'header subtitle...', 
-          headetext: 'header text...',
-          title: 'title ...', 
+          headertilte: 'header title5...', 
+          headersubtilte: 'header subtitle5...', 
+          headetext: 'header text5...',
+          title: 'title5 ...', 
           description: 'description ...',
           img: './img/thirdpic.jpg',
           maps: 'Address'
      },
      {
           id: 6,
-          headertilte: 'header title...', 
-          headersubtilte: 'header subtitle...', 
-          headetext: 'header text...',
-          title: 'title ...', 
+          headertilte: 'header title5...', 
+          headersubtilte: 'header subtitle5...', 
+          headetext: 'header text5...',
+          title: 'title 5...', 
           description: 'description ...',
           img: './img/fouthpic.jpg',
           maps: 'Address'
@@ -75,23 +75,23 @@ export const detailInfo = {
 export const news = [
      {
           id:1,
-          newsTitle: 'news Title', 
-          newsText: 'news Text '
+          newsTitle: 'news Title1', 
+          newsText: 'news Text 1'
      },
      {
           id:2,
-          newsTitle: 'news Title', 
-          newsText: 'news Text '
+          newsTitle: 'news Title2', 
+          newsText: 'news Text2 '
      },
      {
           id:3,
-          newsTitle: 'news Title', 
-          newsText: 'news Text '
+          newsTitle: 'news Title2', 
+          newsText: 'news Text 2'
      },
      {
           id:4,
-          newsTitle: 'news Title', 
-          newsText: 'news Text '
+          newsTitle: 'news Title2', 
+          newsText: 'news Text2 '
      },
 ]
 
@@ -104,19 +104,19 @@ export const reviews = [
      },
      {
           id:2,
-          name: 'first Person', 
+          name: 'second Person', 
           avatar: './img/firstavatar.png',
           comment: 'second person comment'
      },
      {
           id:3,
-          name: 'first Person', 
+          name: 'third Person', 
           avatar: './img/firstavatar.png',
           comment: 'third person comment'
      },
      {
           id:4,
-          name: 'first Person', 
+          name: 'fourth Person', 
           avatar: './img/firstavatar.png',
           comment: 'fourth person comment'
      },
