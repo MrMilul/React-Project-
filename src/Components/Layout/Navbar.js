ed28069423a4e6b5b6c3cdb42c5cea930914bd64
Mr.Milul <miladmizani@yahoo.com>
2020-09-02 16:40:34 +0300

    used react icon

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -8,7 +8,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 export default function Navbar() {
      return (
           <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-          <Link className="navbar-brand text-light" to="#"><i className="fab fa-react"></i></Link>
+          {/* <Link className="navbar-brand text-light" to="#"><i className="fab fa-react"></i></Link> */}
+            <a className="navbar-brand text-light" href="https://reactjs.org/" target="_blank">
+              <img src='favicon.ico'/>
+            </a>
           <button className="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
