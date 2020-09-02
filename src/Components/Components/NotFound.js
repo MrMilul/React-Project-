e86f15c8c87377169ec82cfffc63a22de9f3b7e6
Mr.Milul <miladmizani@yahoo.com>
2020-09-02 17:58:15 +0300

    NotFound page is ready.Used history props

--- src/Components/Components/NotFound.js
+++ src/Components/Components/NotFound.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class NotFound extends Component {
+     constructor(){
+          super()
+     }
+     Home = () =>{
+          this.props.history.push('/')
+     }
      render() {
           return (
               
-               <div className='container'>
-                    notfound....
+               <div className='container mt-5'>
+                   <div class="card">
+                         <div class="card-header text-danger"><h1>Page Not Found</h1></div>
+                         <div class="card-body">
+                         <h5 class="card-title ">Sorry, There is no such page</h5>
+                         <button onClick={this.Home}class="btn btn-primary ">Home</button>
+                         </div>
+                    </div>
                </div>
               
           )
