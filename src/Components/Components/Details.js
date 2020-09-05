59f40e4352c41d9e353137354339088f575eedb5
Mr.Milul <miladmizani@yahoo.com>
2020-09-05 16:31:04 +0300

    just review left

--- src/Components/Components/Details.js
+++ src/Components/Components/Details.js
@@ -6,7 +6,8 @@ export default class Details extends Component {
      render() {
           const mystyle = {
                backgroundImage: 'linear-gradient(to bottom, rgba(0,0,0,0.3) , white)',
-          }
+          };
+         
           return (
                <InfoConsumer>
                     {value => {
@@ -31,39 +32,51 @@ export default class Details extends Component {
                                         </div>
                                    </div>
                                    </div>
-                                   <div className='row justify-content-center border bg-dark'>
+                                   <div className='row justify-content-center'>
                                    <a href='#'>
-                                        <i className="fab fa-linkedin-in my-3 mx-5 btn btn-outline-light"></i></a>
+                                        <i className="fab fa-linkedin-in my-3 mx-5 btn btn-outline-dark "></i></a>
                                    <a href='#'>
-                                        <i className="fab fa-facebook-f btn btn-outline-light my-3 mx-5 "></i></a>
+                                        <i className="fab fa-facebook-f btn btn-outline-dark my-3 mx-5 px-3"></i></a>
                                    <a href='#'>
-                                        <i className="fab fa-twitter btn btn-outline-light my-3 mx-5 "></i></a>
+                                        <i className="fab fa-twitter btn btn-outline-dark my-3 mx-5 "></i></a>
                                    <a href='#'>
-                                        <i className="fab fa-instagram btn btn-outline-light my-3 mx-5 "></i></a>                                   
+                                        <i className="fab fa-instagram btn btn-outline-dark my-3 mx-5 "></i></a>                                   
                                    </div>
                                    <div className='container'>
                                    <ul className="nav nav-tabs">
                                         <li className="nav-item">
-                                             <a className="nav-link text-dark font-weight-bold" data-toggle="collapse" href="#collapseExample">
+                                             <a className="nav-link text-dark font-weight-bold" data-toggle="collapse" href="#des">
                                                   Description</a>
                                         </li>
                                         <li className="nav-item">
-                                             <a className="nav-link text-dark font-weight-bold" href="#">
-                                                  Reviw</a>
+                                             <a className="nav-link text-dark font-weight-bold" href="#review" data-toggle="collapse">
+                                                  Reviw
+                                             </a>
                                         </li>
                                         <li className="nav-item ">
-                                              <a className="nav-link text-dark font-weight-bold" href="#">
-                                                   Map</a>
+                                              <a className="nav-link text-dark font-weight-bold" href="#map-container-google-1" data-toggle="collapse">
+                                                   Map
+                                             </a>
                                         </li>
                                    </ul>
-                                   <div className="collapse" id="collapseExample">
+                                   <div className="collapse" id="des">
                                         <div className="card card-body mt-3">
                                        <img src={img}/>
                                         <h4 className='text-center mt-2'> {title}</h4>
                                         <h4 className=' mt-1'> {description}</h4>
                                         </div>
                                    </div>
-                              
+                                   <div className='collapse' id='review'>
+                                       
+                                   </div>
+                                 
+                                   <div id="map-container-google-1" class="z-depth-1-half map-container collapse justify-content-center">
+                                        <iframe src={maps} style={{width:"100%",
+                                         height:"30rem", 
+                                         border:"0", 
+                                         }}></iframe>
+                                   </div>
+
                                    </div>
                               </>
                          )
